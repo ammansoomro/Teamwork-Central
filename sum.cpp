178061a1438cd48229bb695cd0b964c9615f3301
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2023-02-28 12:03:28 +0500

    Added Sum

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+
+    cout << "Enter two numbers: ";
+    
+    cin >> x >> y;
+
+    cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+}
